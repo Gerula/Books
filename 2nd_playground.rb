89f4504de15ef71aeccbd259b4d54e8e649d8730
Gerula <iacobandrei@gmail.com>
2015-05-03 09:55:00 -0700

    Actual Exercise 14

--- 2nd_playground.rb
+++ 2nd_playground.rb
@@ -2,9 +2,16 @@
 # Don't really know or care on how to organize this crap.
 
 first, second, third = ARGV
+user = ARGV.first
+prompt = "$> "
 
 puts "First #{first}"
 puts "Second #{second}"
 puts "Third #{third}"
 
 puts ARGV.inspect
+
+print "username ", prompt
+username = $stdin.gets.chomp # apparently if you use ARGV you need to explicitly specify $stdin
+			     # as it will default to ARGV.first otherwise
+puts "Username:#{username}"
