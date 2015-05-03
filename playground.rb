991f50c12d82315b45694e0116e8c96078b877d6
Gerula <iacobandrei@gmail.com>
2015-05-02 22:28:15 -0700

    Exercise 1-3

---
+++ playground.rb
@@ -0,0 +1,19 @@
+# need to learn Ruby, the UComp book is very interesting but I spend a lot of time
+# trying to figure out all the Ruby wizardry.
+
+# This for exercises from Zed's book
+
+puts "yeah, I know how to print a string"
+
+# addition
+puts 3+5
+# addition with formatting
+puts "3 + 5 = #{3+5}"
+# modulo
+puts 3 % 4
+# comparison
+puts "Is 5 larger than 3? #{5>3}"
+# floating point
+puts 2.001
+puts 2.001 * 2
+
