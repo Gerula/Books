325313282faba7c387ed4df709b7c7e2e74bc3c3
Gerula <iacobandrei@gmail.com>
2015-05-03 09:49:17 -0700

    Exercise 14

---
+++ 2nd_playground.rb
@@ -0,0 +1,10 @@
+# The first one is getting big so created another one.
+# Don't really know or care on how to organize this crap.
+
+first, second, third = ARGV
+
+puts "First #{first}"
+puts "Second #{second}"
+puts "Third #{third}"
+
+puts ARGV.inspect
