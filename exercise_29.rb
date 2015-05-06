53ce7ee8213e91799cddd5b3fba5d04f20056847
Gerula <iacobandrei@gmail.com>
2015-05-05 22:01:23 -0700

    Exercise 29-31. wow this book is actually intended for people who can't code at all

---
+++ exercise_29.rb
@@ -0,0 +1,16 @@
+people = 100
+cats = 40
+dogs = 50
+
+if (people>cats) 
+	puts "More people than cats"
+else
+	puts "More cats than people"
+end
+
+if dogs > cats
+	puts "More doges than lolcats"
+elsif cats > people
+	puts "More cats than peoples"
+end
+
