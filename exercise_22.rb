853a1559382079203da5bb26fea3d28eb9f34a67
Gerula <iacobandrei@gmail.com>
2015-05-05 18:07:17 -0700

    Exercise 22

---
+++ exercise_22.rb
@@ -0,0 +1,3 @@
+#First, go back through every exercise you have done so far and write down every word and symbol (another name for "character") that you have used. Make sure your list of symbols is complete.
+
+puts "Fuck off, Zed. No."
