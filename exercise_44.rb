04540efdc8d8c08b63ec48bd9ebd72eafdcdd65c
Gerula <iacobandrei@gmail.com>
2015-05-07 19:04:09 -0700

    Exercise 41 Composition

--- exercise_44.rb
+++ exercise_44.rb
@@ -5,15 +5,23 @@ class Animal
 end
 
 class Human < Animal
+	def initialize
+		@kids = []
+	end
+
 	def speak
-		super
 		puts "I am human"
 	end
+
+	def give_birth(kid)
+		@kids.push(kid)
+	end
+
+	attr_accessor :kids
 end
 
 class AssHole < Human
 	def speak
-		super
 		puts "Fuck off!"
 	end
 end
@@ -21,3 +29,11 @@ end
 puts AssHole.new.speak
 puts Human.new.speak
 puts Animal.new.speak
+asshole = AssHole.new
+asshole.give_birth(AssHole.new)
+asshole.give_birth(Human.new)
+
+asshole.kids.inspect
+
+puts "Kids speaking now"
+asshole.kids.each {|kid| kid.speak}
