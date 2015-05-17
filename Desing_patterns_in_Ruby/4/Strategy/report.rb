547f47f1ff3840b66c7b55a2223629852ceb1ae4
Gerula <iacobandrei@gmail.com>
2015-05-17 10:24:43 -0700

    Strategy

---
+++ Desing_patterns_in_Ruby/4/Strategy/report.rb
@@ -0,0 +1,48 @@
+class Formatter
+    def output_report(title, text)
+        raise NotImplementedError, "Not implemented"
+    end
+end
+
+class Html_formatter < Formatter
+    def output_report(title, text)
+        puts "<html>"
+        puts "  <head>"
+        puts "      <title>#{title}</title>"
+        puts "  </head>"
+        puts "  <body>"
+        text.each { |x|
+            puts "      <p> #{x} </p>"
+        }
+
+        puts "  </body>"
+        puts "</html>"
+    end
+end
+
+class Text_formatter < Formatter
+    def output_report(title, text)
+        puts "--- Begin report ---"
+        puts " Title #{title}"
+        text.each { |x|
+            puts x
+        }
+   end
+end
+
+class Report
+    def initialize(formatter, title, text)
+        @formatter = formatter
+        @title = title
+        @text = text
+    end
+
+    def print
+        @formatter.output_report(@title, @text)
+    end
+end
+
+report = Report.new(Text_formatter.new, "Status", ["Everything sucks", "badly"])
+report.print
+report = Report.new(Html_formatter.new, "Status", ["Everything sucks", "badly"])
+report.print
