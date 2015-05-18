4ee32f01487c37c92b61e5f7093e38e7d1164baa
Gerula <iacobandrei@gmail.com>
2015-05-18 07:00:30 -0700

    Iterator both internal and external

---
+++ Desing_patterns_in_Ruby/7/Iterator/external_iterator.rb
@@ -0,0 +1,36 @@
+# provides a way of sequentially accessing the elements of an aggregate object without exposing its underlying representation
+
+# External iterator
+
+class ArrayIterator
+    def initialize(array)
+        @array = array
+        @index = 0
+    end
+
+    def has_next?
+        @index < @array.length
+    end
+
+    def item
+        @array[@index]
+    end
+
+    def next_item
+        value = item
+        @index += 1
+        value
+    end
+end
+
+it = ArrayIterator.new([1, 2, 3, 4, 5, 6])
+
+while it.has_next?
+    puts "Item #{it.next_item}"
+end
+
+it = ArrayIterator.new("abcdefghij")
+
+while it.has_next?
+    puts "Item #{it.next_item}"
+end
