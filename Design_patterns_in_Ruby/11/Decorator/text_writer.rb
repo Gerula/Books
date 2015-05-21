f957c939ffb16a9873a1fb30bc3af7231434684c
Gerula <iacobandrei@gmail.com>
2015-05-21 10:28:42 -0700

    Decorator with forwardable

--- Design_patterns_in_Ruby/11/Decorator/text_writer.rb
+++ Design_patterns_in_Ruby/11/Decorator/text_writer.rb
@@ -1,22 +1,12 @@
+require 'forwardable'
+
 class Writer_decorator
+    extend Forwardable
+
+    def_delegators :@real_writer, :write_line, :rewind, :pos, :close
+
     def initialize(writer)
         @writer = writer
-    end
-
-    def write_line(line)
-        @writer.write_line(line)
-    end
-
-    def pos
-        @writer.pos
-    end
-
-    def rewind
-        @writer.rewind
-    end
-
-    def close
-        @writer.close
     end
 end
 
