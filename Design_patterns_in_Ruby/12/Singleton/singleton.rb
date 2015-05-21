b003d44be9cced19ca14c3b8b7c5010e832b7e86
Gerula <iacobandrei@gmail.com>
2015-05-21 14:06:28 -0700

    Static methods

--- Design_patterns_in_Ruby/12/Singleton/singleton.rb
+++ Design_patterns_in_Ruby/12/Singleton/singleton.rb
@@ -13,10 +13,21 @@ class VariableTester
     def to_s
         "Instance:#{@instance_count} Class:#{@@class_count}"
     end
+
+    def self.static_method
+        puts "#{self}"
+    end
+
+    def method_missing(name, *args)
+        puts "WTF?"
+    end
 end
 
 [VariableTester.new, VariableTester.new, VariableTester.new].each {|x|
     x.increment
     puts x
+    x.static_method
 }
+
+VariableTester.static_method
 
