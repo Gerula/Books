8e6b7dd66176cd6860d7139384c442cc8108546b
Gerula <iacobandrei@gmail.com>
2015-05-22 16:50:29 -0700

    Interpreter - file expression DSL take 1 - operations

---
+++ Design_patterns_in_Ruby/15/Interpreter/file_utils.rb
@@ -0,0 +1,86 @@
+require 'find'
+
+class Expression
+end
+
+class All < Expression
+    def evaluate(dir)
+        results = []
+        Find.find(dir) { |x|
+            next unless File.file?(x)
+            results << x
+        }
+
+        return results
+    end
+end
+
+class FileName < Expression
+    def initialize(pattern)
+        @pattern = pattern
+    end
+    
+    def evaluate(dir)
+        results = []
+        Find.find(dir) { |x|
+            next unless File.file?(x)
+            name = File.basename(x)
+            results << x if File.fnmatch(@pattern, name)
+        }
+
+        return results
+    end
+end
+
+class BigFile < Expression
+    def initialize(size)
+        @size = size
+    end
+    
+    def evaluate(dir)
+        results = []
+        Find.find(dir) { |x|
+            next unless File.file?(x)
+            results << x if File.size(x) > @size
+        }
+
+        return results
+    end
+
+end
+
+class Not < Expression
+    def initialize(expression)
+        @expression = expression
+    end
+
+    def evaluate(dir)
+        All.new.evaluate(dir) - @expression.evaluate(dir)
+    end
+end
+
+class Or < Expression
+    def initialize(expression1, expression2)
+        @expression1 = expression1
+        @expression2 = expression2
+    end
+
+    def evaluate(dir)
+        @expresion1.evaluate(dir) | @expression2.evaluate(dir)
+    end
+end
+
+class And < Expression
+    def initialize(expression1, expression2)
+        @expression1 = expression1
+        @expression2 = expression2
+    end
+
+    def evaluate(dir)
+        @expression1.evaluate(dir) & @expression2.evaluate(dir)
+    end
+end
+
+
+puts FileName.new("*.rb").evaluate("../../../")
+puts And.new(Not.new(BigFile.new(100)),FileName.new("*master")).evaluate("../../../")
