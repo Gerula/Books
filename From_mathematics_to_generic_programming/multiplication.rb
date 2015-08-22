84569cf6f27937c8e71cc01b5c5f07314d60cd3a
Gerula <iacobandrei@gmail.com>
2015-08-21 20:54:23 -0700

    Egyptian | Russian peasant multiplication

--- From_mathematics_to_generic_programming/multiplication.rb
+++ From_mathematics_to_generic_programming/multiplication.rb
@@ -9,6 +9,19 @@ class Fixnum
             return self + self * (other - 1)
         end
     end
+
+    def pleb(other)
+        small = [self, other].min
+        large = [self, other].max
+        result = 0
+        while small != 0
+            small, remainder = small.divmod(2)
+            result += remainder == 0 ? 0 : large
+            large *= 2
+        end
+
+        return result
+    end
 end
 
 assert_equal(12, 6 * 2)
@@ -16,3 +29,8 @@ assert_equal(12, 2 * 6)
 assert_equal(18, 3 * 6)
 assert_equal(80, 10 * 8)
 assert_equal(81, 9 * 9)
+assert_equal(12, 6.pleb(2))
+assert_equal(12, 2.pleb(6))
+assert_equal(18, 3.pleb(6))
+assert_equal(80, 10.pleb(8))
+assert_equal(81, 9.pleb(9))
