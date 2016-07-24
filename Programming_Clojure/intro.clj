0bfdfcc4b196fe23b8a79435e56d7786d7c84faa
Gerula <iacobandrei@gmail.com>
2016-07-24 11:16:31 -0700

    mend

--- Programming_Clojure/intro.clj
+++ Programming_Clojure/intro.clj
@@ -1,8 +1,8 @@
 (def visitors (atom #{}))
 
 (defn hello
-  ;;; Takes a name and says hello.
-  ;;; Also keeps track if you've been here before.
+  "Takes a name and says hello.
+   Also keeps track if you've been here before."
   [name]
   (swap! visitors conj name)
   (str "Hello " name))
